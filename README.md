24eac05b896e395d3d6efe80fc249efc272e3e94
Rhet Turnbull <rturnbull@gmail.com>
2022-03-06 08:43:56 -0800

    First commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# Visualize the color palette of a VS Code color theme
+
+## Background
+
+I'm using the wonderful [rich](https://github.com/Textualize/rich) python package to beautify the output of some of my python command line [apps](https://github.com/RhetTbull/osxphotos).  I'm not a designer and wanted to pick colors that look good but also wanted to provide themes for dark mode, light mode, as well as color-blind themes.  VS Code has lots of beautiful color themes so why not just use the color palette of one of them?  This script reads a [VS Code color theme](https://code.visualstudio.com/docs/getstarted/themes) JSON file and prints out a pretty palette to the terminal with the rgb values ready to drop into a rich [Style](https://rich.readthedocs.io/en/stable/style.html) object.
+
+## Example
+
+`python3 theme_colors.py vscode_github_theme_light.json`
+
+![](images/vscode_github_light.png)
+
+`python3 theme_colors.py dark_theme_color_blind.json`
+
+![](images/dark_theme_color_blind.png)
+
+
+## Installation
+
+- `pip install -r requirements.txt`
+- `python3 theme_colors.py theme_file.json`
+
+# License
+
+MIT License. Do whatever you want with this. No Warranty.
---
+++ theme_colors.py
@@ -0,0 +1,90 @@
+"""Print theme color palette from a VS Code theme file."""
+
+import json5
+from typing import List, Tuple
+
+import typer
+from PIL import ImageColor
+from rich.console import Console
+from rich.style import Style
+from rich.table import Table
+from rich.text import Text
+
+
+def colors(theme_file: str):
+    """Print theme color palette from a VS Code theme file."""
+    colors = get_colors_from_json_theme(theme_file)
+
+    table = Table(show_header=False)
+    table.add_column("Color")
+    table.add_column("Hex")
+    table.add_column("RGB")
+    rows = [
+        (bar(10, color), color, rich_rgb_str(hex_to_rgb(color))) for color in colors
+    ]
+    rows.sort(key=lambda x: x[2])
+    for row in rows:
+        table.add_row(*row)
+    console = Console()
+    console.print(table)
+
+
+def get_colors_from_json_theme(theme_file: str) -> List[str]:
+    with open(theme_file, "r") as f:
+        # use json5 to parse the theme file as it supports comments and trailing commas
+        # which are prevalent in VS Code theme files
+        theme = json5.load(f)
+
+    # colors in the "colors" section then in the "tokenColors" section
+    colors = [v for k, v in theme.get("colors", []).items()]
+    for token in theme.get("tokenColors", []):
+        if "settings" in token:
+            try:
+                colors.append(token["settings"]["foreground"])
+            except KeyError:
+                pass
+    colors = drop_alpha(colors)
+    colors = list(set(colors))
+    return colors
+
+
+def drop_alpha(colors: List[str]) -> List[str]:
+    """Remove alpha value from color strings."""
+    # drop the alpha channel if present
+    new_colors = []
+    for color in colors:
+        if len(color) == 9:
+            # #RRGGBBAA
+            color = color[:7]
+        elif len(color) == 4:
+            # #RGBA
+            color = color[:3]
+        new_colors.append(color)
+    return new_colors
+
+
+def hex_to_rgb(color: str) -> Tuple[int, int, int]:
+    """Get RGB color value from hex color string in format #RGB, #RGBA, #RRGGBB, or #RRGGBBAA"""
+    return ImageColor.getrgb(color)
+
+
+def bar(length: int, color: str) -> str:
+    """Create a color bar."""
+    bar = "█" * length
+    color = hex_to_rgb(color)
+    return Text(bar, style=Style(color=rich_rgb_str(color)))
+
+
+def rich_rgb_str(rgb: Tuple[int, int, int]) -> str:
+    """Convert RGB tuple to string with RGB values separated by commas."""
+    # drop alpha value if present
+    return "rgb(" + ",".join(str(c) for c in rgb[:3]) + ")"
+
+
+def main(theme_file: str = typer.Argument(..., help="Path to VS Code theme file")):
+    """Print theme color palette from a VS Code theme file."""
+    colors(theme_file)
+
+
+if __name__ == "__main__":
+    typer.run(main)
